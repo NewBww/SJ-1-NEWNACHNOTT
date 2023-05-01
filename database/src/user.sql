4c8c8f50f21878b578e4cdc18391c6738d013cd5
Bowonwit Anothaisintavee <Bowonwit.A@gmail.com>
2023-05-01 22:38:30 +0700

    update db user scripts

--- database/src/user.sql
+++ database/src/user.sql
@@ -1,13 +1,14 @@
 use sas;
+DROP USER IF EXISTS 'dba_new'@'localhost', 'dba_new'@'%', 'dba_nach'@'localhost', 'dba_nach'@'%', 'dba_nott'@'localhost', 'dba_nott'@'%', 'api_announcement'@'localhost';
 create user 'dba_new'@'localhost' identified by '3n';
+create user 'dba_new'@'%' identified by '3n';
 create user 'dba_nach'@'localhost' identified by '3n';
+create user 'dba_nach'@'%' identified by '3n';
 create user 'dba_nott'@'localhost' identified by '3n';
+create user 'dba_nott'@'%' identified by '3n';
 create user 'api_announcement'@'localhost' identified by 'sj1sas';
 
-grant alter, create, create view, delete, drop, index, insert, lock tables, select, show view, update  on *.* to 'dba_new'@'localhost', 
-'dba_nach'@'localhost', 'dba_nott'@'localhost' ;
+grant alter, create, create view, delete, drop, index, insert, lock tables, select, show view, update  on *.* to 'dba_new'@'localhost', 'dba_nach'@'localhost', 'dba_nott'@'localhost' ;
+grant alter, create, create view, delete, drop, index, insert, lock tables, select, show view, update  on *.* to 'dba_new'@'%', 'dba_nach'@'%', 'dba_nott'@'%' ;
 
 grant insert, update, delete, select on *.* to 'api_announcement'@'localhost';
-
-show grants for 'api_announcement'@'localhost';
-show grants for 'dba_new'@'localhost';
