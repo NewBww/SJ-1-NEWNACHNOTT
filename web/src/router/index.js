fd5a2744d28b59ec5b95d78d337bdf6819b532ed
Nach <benten599@hotmail.com>
2023-05-01 15:00:53 +0700

    update router

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -4,11 +4,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      redirect: { name: 'admin-announcement-listing' }
-
-    },
-    {
       path: '/admin/announcement',
       name: 'admin-announcement-listing',
       component: () => import('@/components/pages/AdminAnnouncementListingPage.vue'),
