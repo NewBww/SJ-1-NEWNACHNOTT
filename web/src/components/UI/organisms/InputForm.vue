0b0378b1d8c8fe28ada3cc128160ed75b9b2f0ac
pongsapark <pongsapak.ratt@outlook.com>
2023-05-05 17:47:20 +0700

    update services

---
+++ web/src/components/UI/organisms/InputForm.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TitleInput from '@/components/UI/molecules/TitleInput.vue'
+import PublishDate from '@/components/UI/molecules/PublishDate.vue'
+import DescriptionInput from '@/components/UI/molecules/DescriptionInput.vue'
+import DisplayCheckBox from '@/components/UI/molecules/DisplayCheckBox.vue'
+import CategoryInput from '@/components/UI/molecules/CategoryInput.vue'
+import CloseDate from '@/components/UI/molecules/CloseDate.vue'
+
+// const newAnnouncement = {}
+</script>
+
+<template>
+  <TitleInput />
+  <CategoryInput />
+  <DescriptionInput />
+  <PublishDate />
+  <CloseDate />
+  <DisplayCheckBox />
+</template>
+
+<style scoped></style>
