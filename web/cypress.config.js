3d93970300d4547330e5498955912ab2cdb2293d
Nach <benten599@hotmail.com>
2023-05-14 16:31:24 +0700

    config cypress

--- web/cypress.config.js
+++ web/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
+    // baseUrl: 'http://intproj22.sit.kmutt.ac.th/sj1/'
     baseUrl: 'http://localhost:4173'
   }
 })
