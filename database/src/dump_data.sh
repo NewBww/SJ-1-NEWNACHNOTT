49a2ba33f8cbd3ac47125ffc1b553b60dc37dd95
Nach <benten599@hotmail.com>
2023-06-02 00:52:57 +0700

    modify bash shell script

--- database/src/dump_data.sh
+++ database/src/dump_data.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cat sas.sql | docker exec -i sas-database-1 mysql -u root -phThKPEq93f
+cat user.sql | | docker exec -i sas-database-1 mysql -u root -phThKPEq93f 
 cat sprint3_automate_test.sql | docker exec -i sas-database-1 mysql -u root -phThKPEq93f sas
