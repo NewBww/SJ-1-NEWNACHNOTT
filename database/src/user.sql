bc295bbadba14f7616759f47da31715754cfac65
Bowonwit Anothaisintavee <Bowonwit.A@gmail.com>
2023-08-17 23:37:45 +0700

    modify user: add bung

--- database/src/user.sql
+++ database/src/user.sql
@@ -1,14 +1,14 @@
 use sas;
-DROP USER IF EXISTS 'dba_new'@'localhost', 'dba_new'@'%', 'dba_nach'@'localhost', 'dba_nach'@'%', 'dba_nott'@'localhost', 'dba_nott'@'%', 'api_announcement'@'localhost';
+DROP USER IF EXISTS 'dba_new'@'localhost', 'dba_new'@'%', 'dba_nach'@'localhost', 'dba_nach'@'%', 'dba_bung'@'localhost', 'dba_bung'@'%', 'api_announcement'@'localhost';
 create user 'dba_new'@'localhost' identified by '3n';
 create user 'dba_new'@'%' identified by '3n';
 create user 'dba_nach'@'localhost' identified by '3n';
 create user 'dba_nach'@'%' identified by '3n';
-create user 'dba_nott'@'localhost' identified by '3n';
-create user 'dba_nott'@'%' identified by '3n';
+create user 'dba_bung'@'localhost' identified by '1234';
+create user 'dba_bung'@'%' identified by '1234';
 create user 'api_announcement'@'localhost' identified by 'sj1sas';
 
-grant alter, create, create view, delete, drop, index, insert, lock tables, select, show view, update  on *.* to 'dba_new'@'localhost', 'dba_nach'@'localhost', 'dba_nott'@'localhost' ;
-grant alter, create, create view, delete, drop, index, insert, lock tables, select, show view, update  on *.* to 'dba_new'@'%', 'dba_nach'@'%', 'dba_nott'@'%' ;
+grant alter, create, create view, delete, drop, index, insert, lock tables, select, show view, update  on *.* to 'dba_new'@'localhost', 'dba_nach'@'localhost', 'dba_bung'@'localhost' ;
+grant alter, create, create view, delete, drop, index, insert, lock tables, select, show view, update  on *.* to 'dba_new'@'%', 'dba_nach'@'%', 'dba_bung'@'%' ;
 
 grant insert, update, delete, select on *.* to 'api_announcement'@'localhost';
